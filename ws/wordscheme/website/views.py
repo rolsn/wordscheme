3d03b2e75a389e394a8a25923476de1d5dc4b915
rj <rj@eth3.org>
2016-06-08 18:15:49 -0400

    use a template for index view

--- ws/wordscheme/website/views.py
+++ ws/wordscheme/website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("home.")
+    return render(request, 'website/index.html', {})
