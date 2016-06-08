f1c2b5dc72bdd60cdeefd5e70aae3822657336d5
rj <rj@eth3.org>
2016-06-08 14:25:39 -0400

    a unit test for users

--- ws/wordscheme/users/tests.py
+++ ws/wordscheme/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from users.models import Users
+
+def create_user():
+    now = timezone.now()
+
+    Users.objects.create(
+        username="lola", 
+        password="764efa883ddc1e",
+        reg_date=now,
+        email="lola@example.com"
+        )
+
+class UsersTestCase(TestCase):
+    def test_user_made(self):
+        create_user()
+        user = Users.objects.get(username="lola")
+        self.assertEqual(user.username, "lola")
