f006d3e1137e5f6a98e87242fefde785b900bb6c
rj <rj@eth3.org>
2016-06-08 15:54:32 -0400

    these fields are unique

--- ws/wordscheme/users/models.py
+++ ws/wordscheme/users/models.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Users(models.Model):
-    username    = models.CharField(max_length=32)
+    username    = models.CharField(max_length=32, unique=True)
     password    = models.CharField(max_length=64)
     reg_date    = models.DateTimeField('Registration Date')
-    email       = models.EmailField()
+    email       = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
