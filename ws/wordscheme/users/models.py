1c9509bd6e75f21963b05c61ff626a3ad757993c
rj <rj@eth3.org>
2016-06-08 03:36:34 -0400

    db setup

--- ws/wordscheme/users/models.py
+++ ws/wordscheme/users/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username    = models.CharField(max_length=32)
+    password    = models.CharField(max_length=64)
+    reg_date    = models.DateTimeField('Registration Date')
+    email       = models.EmailField()
