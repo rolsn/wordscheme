9303b9fada70879c9f8bbe25f19805b398d1e9f7
rj <rj@eth3.org>
2016-06-10 16:18:27 -0400

    adding new models

--- ws/wordscheme/website/models.py
+++ ws/wordscheme/website/models.py
@@ -2,10 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from users.models import Users
+from django.contrib.auth.models import User
 
 class Articles(models.Model):
-    user_id         = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_id         = models.ForeignKey(User, on_delete=models.CASCADE)
     date            = models.DateTimeField('Publication Date')
     article_text    = models.TextField()
     subject         = models.CharField(max_length=64)
+
+class Comments(models.Model):
+    user_id         = models.ForeignKey(User, on_delete=models.CASCADE)
+    art_id          = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    date            = models.DateTimeField('Comment Date')
+    comment_text    = models.TextField()
+
+class Ratings(models.Model):
+    user_id         = models.ForeignKey(User, on_delete=models.CASCADE)
+    art_id          = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    rating          = models.SmallIntegerField()
