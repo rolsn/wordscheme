6c5ef7e90886df461f0e47cda1b5d5092f66572c
rj <rj@eth3.org>
2016-06-10 12:47:15 -0400

    using the auth system now

--- ws/wordscheme/users/views.py
+++ ws/wordscheme/users/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
 from django.utils import timezone
 
-from users.models import Users
+from django.contrib.auth.models import User
+from users.forms import RegistrationForm
 
 @require_http_methods(["GET", "POST"])
 def new_registration(request):
-    method = request.method
-    
-    if method == "POST":
+    if request.method == "POST":
+        regForm = RegistrationForm(request.POST)
+
+        if not regForm.is_valid():
+            return render(request, 'users/register.html', {'form': RegistrationForm().as_ul(), 'error': regForm.errors})
+
         try:
             params = {
                     "username": request.POST['username'],
@@ -19,15 +23,18 @@ def new_registration(request):
         except KeyError:
             return HttpResponse("Invalid input.")
 
-        userExists = True if len(Users.objects.filter(username=params['username'])) == 1 else False
-        emailExists = True if len(Users.objects.filter(email=params['email'])) == 1 else False
+        userExists = True if len(User.objects.filter(username=params['username'])) == 1 else False
+        emailExists = True if len(User.objects.filter(email=params['email'])) == 1 else False
 
         if userExists:
             return HttpResponse("Username already taken.")
         if emailExists:
             return HttpResponse("Email already registered.")
 
-        params['reg_date'] = timezone.now()
-        Users.objects.create(**params)
+        #params['reg_date'] = timezone.now()
+        User.objects.create(**params)
 
-    return render(request, 'users/register.html', {})
+        return HttpResponse("User %s created." % params['username'])
+
+    if request.method == "GET":
+        return render(request, 'users/register.html', {'form': RegistrationForm().as_ul()})
