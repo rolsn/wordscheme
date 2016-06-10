8ef7266d7f4a8280b19997346c2901f27c13578b
rj <rj@eth3.org>
2016-06-10 17:13:41 -0400

    namespacing the urls for website

--- ws/wordscheme/website/urls.py
+++ ws/wordscheme/website/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name="website"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/', views.new_registration, name='register'),
