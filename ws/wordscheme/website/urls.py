5a2aef81cd0b8fdf905c80b154e9a221e9ead0c2
rj <rj@eth3.org>
2016-06-10 21:14:59 -0400

    removing beginning-of-line character from url regexes

--- ws/wordscheme/website/urls.py
+++ ws/wordscheme/website/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name="website"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/', views.new_registration, name='register'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^main/', views.main, name='main'),
+    url(r'register/', views.new_registration, name='register'),
+    url(r'login/', views.login, name='login'),
+    url(r'main/', views.main, name='main'),
     url(r'^articles/(?P<id>[0-9]+)/$', views.article, name='articles'),
     url(r'^new/', views.new_article, name='new_article'),
     url(r'^logout/', logout_then_login),
