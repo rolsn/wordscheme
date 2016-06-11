71749f7ec591a38529686864c95d639389b9b626
rj <rj@eth3.org>
2016-06-11 00:21:47 -0400

    hide password input fields

--- ws/wordscheme/website/forms.py
+++ ws/wordscheme/website/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import PasswordInput, TextInput
 from django.contrib.auth.models import User
 
 from models import Articles, Comments
@@ -7,11 +8,17 @@ class RegistrationForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
+        widgets = {
+                'password': PasswordInput()
+                }
 
 class LoginForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+        widgets = {
+                'password' : PasswordInput()
+                }
 
 class ArticleForm(ModelForm):
     class Meta:
