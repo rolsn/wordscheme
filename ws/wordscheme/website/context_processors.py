ce7129c931f2857b8d0536b967426b3e66659c73
rj <rj@eth3.org>
2016-06-14 21:39:00 -0400

    removing comment

--- ws/wordscheme/website/context_processors.py
+++ ws/wordscheme/website/context_processors.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 
 def default(request):
-    print "wtf", request.user.is_authenticated()
     return {
             "current_datetime"      : timezone.now(),
             "current_user"          : request.user
