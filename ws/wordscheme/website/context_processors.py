a6f4806beb4271727c967ff20c465f40e3447afc
rj <rj@eth3.org>
2016-06-14 21:21:58 -0400

    adding a navbar and showing it only to logged in users

--- ws/wordscheme/website/context_processors.py
+++ ws/wordscheme/website/context_processors.py
@@ -1,8 +1,9 @@
 from django.utils import timezone
 
 def default(request):
+    print "wtf", request.user.is_authenticated()
     return {
-            current_date    = timezone.now(),
-            username        = request.user()
+            "current_datetime"      : timezone.now(),
+            "current_user"          : request.user
             }
 
