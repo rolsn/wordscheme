d967d8efeea226bb6c8e8c4cd22c6b2313829566
rj <rj@eth3.org>
2016-06-30 15:16:27 -0400

    adding a template helper for getting array lengths

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -3,3 +3,7 @@ from django.template.defaulttags import register
 @register.filter(name='lookup')
 def get_item(d, k):
     return d.get(k)
+
+@register.filter(name='length')
+def get_length(a):
+    return len(a)
